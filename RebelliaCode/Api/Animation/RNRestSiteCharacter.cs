042c632b8b824ba0fea28fe6b245b7ca584911b0
青白影 <376565306@qq.com>
2026-05-11 16:34:03 +0800

    Update RNRestSiteCharacter.cs

--- RebelliaCode/Api/Animation/RNRestSiteCharacter.cs
+++ RebelliaCode/Api/Animation/RNRestSiteCharacter.cs
@@ -2,5 +2,6 @@ using Godot;
 using MegaCrit.Sts2.Core.Nodes.RestSite;
 
 namespace Rebellia.RebelliaCode.Api.Animation;
+
 [GlobalClass]
 public partial class RNRestSiteCharacter : NRestSiteCharacter { }
