13f965ad87c0701adbdf692834f98052a596cc2f
青白影 <376565306@qq.com>
2026-06-12 16:33:01 +0800

    part1
    增加了当前回合血剑费用，增加和花费，以及当前最大上限的记录

---
+++ RebelliaCode/Cards/Uncommon/BloodCrimsonAmbush.cs
@@ -0,0 +1,56 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using Rebellia.RebelliaCode.Api;
+using Rebellia.RebelliaCode.Api.Cards;
+using Rebellia.RebelliaCode.Powers;
+
+namespace Rebellia.RebelliaCode.Cards.Uncommon;
+
+public class BloodCrimsonAmbush()
+    : RebelliaCard(0, CardType.Attack, CardRarity.Uncommon, TargetType.RandomEnemy)
+{
+    private const string HitCountKey = "TotalHits";
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipsValue.BloodSwordArt];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+        [
+            new DamageVar(5m, ValueProp.Move),
+            new CalculationBaseVar(1),
+            new CalculationExtraVar(1),
+            new CalculatedVar(HitCountKey).WithMultiplier(
+                (card, target) =>
+                {
+                    var bloodPower = card.Owner?.Creature?.GetPower<BloodSwordArtPower>();
+                    if (bloodPower == null)
+                        return 0m;
+                    return bloodPower.GetGainedThisTurn() + bloodPower.GetSpentThisTurn();
+                }
+            ),
+        ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        var target = play.Target;
+        if (target == null)
+            return;
+
+        var calcVar = DynamicVars[HitCountKey] as CalculatedVar;
+        int hitCount = (int)(calcVar?.Calculate(target) ?? 1m);
+        if (hitCount < 1)
+            hitCount = 1;
+
+        decimal damage = DynamicVars.Damage.BaseValue;
+        var cmd = DamageCmd.Attack(damage).FromCard(this).Targeting(target).WithHitCount(hitCount);
+        await cmd.Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3m);
+    }
+}
--- RebelliaCode/Powers/BloodSwordArtPower.cs
+++ RebelliaCode/Powers/BloodSwordArtPower.cs
@@ -1,6 +1,8 @@
-using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
-using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using Rebellia.RebelliaCode.Api.Powers;
 
 namespace Rebellia.RebelliaCode.Powers;
@@ -9,10 +11,25 @@ public class BloodSwordArtPower : RebelliaPowers
 {
     public int BloodArtMaxPoints { get; set; } = 2;
 
+    private int _gainedThisTurn;
+    private int _spentThisTurn;
+
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
     public override int DisplayAmount => GetInternalData<Data>().BloodArtPoints;
     public override bool ShouldReceiveCombatHooks => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars
+    {
+        get
+        {
+            yield return new DynamicVar("GainedThisTurn", 0);
+            yield return new DynamicVar("SpentThisTurn", 0);
+            yield return new DynamicVar("MaxPoints", BloodArtMaxPoints);
+            foreach (var v in base.CanonicalVars)
+                yield return v;
+        }
+    }
 
     protected override object InitInternalData()
     {
@@ -21,29 +38,67 @@ public class BloodSwordArtPower : RebelliaPowers
 
     public void AddPoints(int amount)
     {
+        if (amount <= 0)
+            return;
         var data = GetInternalData<Data>();
-        data.BloodArtPoints = Math.Min(data.BloodArtPoints + amount, BloodArtMaxPoints);
-        InvokeDisplayAmountChanged();
+        int newPoints = Math.Min(data.BloodArtPoints + amount, BloodArtMaxPoints);
+        int actualGain = newPoints - data.BloodArtPoints;
+        if (actualGain > 0)
+        {
+            data.BloodArtPoints = newPoints;
+            _gainedThisTurn += actualGain;
+
+            DynamicVars["GainedThisTurn"].BaseValue = _gainedThisTurn;
+            InvokeDisplayAmountChanged();
+        }
     }
 
     public bool TrySpendPoints(int amount)
     {
+        if (amount <= 0)
+            return true;
         var data = GetInternalData<Data>();
         if (data.BloodArtPoints < amount)
             return false;
         data.BloodArtPoints -= amount;
+        _spentThisTurn += amount;
+
+        DynamicVars["SpentThisTurn"].BaseValue = _spentThisTurn;
         InvokeDisplayAmountChanged();
         return true;
     }
 
-    public int GetPoints()
+    public int GetPoints() => GetInternalData<Data>().BloodArtPoints;
+
+    public int GetGainedThisTurn() => _gainedThisTurn;
+
+    public int GetSpentThisTurn() => _spentThisTurn;
+
+    public void SetMaxPoints(int newMax)
     {
-        return GetInternalData<Data>().BloodArtPoints;
+        BloodArtMaxPoints = newMax;
+        if (DynamicVars.ContainsKey("MaxPoints"))
+            DynamicVars["MaxPoints"].BaseValue = newMax;
     }
 
-    public override async Task AfterCombatEnd(CombatRoom room)
+    public override async Task BeforeSideTurnStart(
+        PlayerChoiceContext choiceContext,
+        CombatSide side,
+        IReadOnlyList<Creature> participants,
+        ICombatState combatState
+    )
     {
-        await PowerCmd.Remove(this);
+        if (participants.Contains(Owner))
+        {
+            _gainedThisTurn = 0;
+            _spentThisTurn = 0;
+
+            if (DynamicVars.ContainsKey("GainedThisTurn"))
+                DynamicVars["GainedThisTurn"].BaseValue = 0;
+            if (DynamicVars.ContainsKey("SpentThisTurn"))
+                DynamicVars["SpentThisTurn"].BaseValue = 0;
+        }
+        await Task.CompletedTask;
     }
 
     private class Data
