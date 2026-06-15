3c0d49f619af4ff2af93781c6dbd3d955cd34cfc
青白影 <376565306@qq.com>
2026-06-15 20:45:49 +0800

    loodCrimsonLink卡牌

---
+++ RebelliaCode/Cards/Uncommon/BloodCrimsonLink.cs
@@ -0,0 +1,46 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using Rebellia.RebelliaCode.Api;
+using Rebellia.RebelliaCode.Api.Cards;
+using Rebellia.RebelliaCode.Powers.cards;
+
+namespace Rebellia.RebelliaCode.Cards.Uncommon;
+
+public class BloodCrimsonLink()
+    : RebelliaCard(2, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+        [
+            new PowerVar<BloodCrimsonLinkPower>(1),
+            new PowerVar<DexterityPower>(2),
+            new PowerVar<StrengthPower>(2),
+        ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await Utils.GivePower<DexterityPower>(
+            choiceContext,
+            Owner.Creature,
+            DynamicVars,
+            Owner.Creature,
+            this
+        );
+        await Utils.GivePower<StrengthPower>(
+            choiceContext,
+            Owner.Creature,
+            DynamicVars,
+            Owner.Creature,
+            this
+        );
+
+        await Utils.GivePower<BloodCrimsonLinkPower>(choiceContext, this, play);
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
+}
