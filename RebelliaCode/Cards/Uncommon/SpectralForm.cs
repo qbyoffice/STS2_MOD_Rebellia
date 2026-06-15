4ee0558a5c2875cf8a7a5fdde5817e58f9466c40
青白影 <376565306@qq.com>
2026-06-15 18:57:25 +0800

    灵质态 SpectralForm

---
+++ RebelliaCode/Cards/Uncommon/SpectralForm.cs
@@ -0,0 +1,54 @@
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using Rebellia.RebelliaCode.Api;
+using Rebellia.RebelliaCode.Api.Cards;
+using Rebellia.RebelliaCode.Powers.cards;
+
+namespace Rebellia.RebelliaCode.Cards.Uncommon;
+
+public class SpectralForm() : RebelliaCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+        [HoverTipFactory.FromPower<IntangiblePower>(), HoverTipsValue.CrimsonVeil];
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+        [new PowerVar<IntangiblePower>(1), new PowerVar<SpectralFormPower>(1)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        var veilPower = Owner.Creature.GetPower<CrimsonVeilPower>();
+        if (veilPower != null)
+        {
+            int currentVeil = veilPower.GetVeilPoints();
+            if (currentVeil > 0)
+                veilPower.AddVeilPoints(-currentVeil);
+        }
+
+        await Utils.GivePower<IntangiblePower>(
+            choiceContext,
+            Owner.Creature,
+            DynamicVars,
+            Owner.Creature,
+            this
+        );
+
+        await Utils.GivePower<SpectralFormPower>(
+            choiceContext,
+            Owner.Creature,
+            DynamicVars,
+            Owner.Creature,
+            this
+        );
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+        RemoveKeyword(CardKeyword.Exhaust);
+    }
+}
---
+++ RebelliaCode/Powers/cards/SpectralFormPower.cs
@@ -0,0 +1,36 @@
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using Rebellia.RebelliaCode.Api.Powers;
+
+namespace Rebellia.RebelliaCode.Powers.cards;
+
+public class SpectralFormPower : RebelliaPowers
+{
+    public override PowerType Type => PowerType.Debuff;
+    public override PowerStackType StackType => PowerStackType.Single;
+    public override bool ShouldReceiveCombatHooks => true;
+
+    public override bool ShouldPlay(CardModel card, AutoPlayType autoPlayType)
+    {
+        if (card.Type == CardType.Attack && card.Owner.Creature == Owner)
+            return false;
+        return true;
+    }
+
+    public override async Task AfterSideTurnEnd(
+        PlayerChoiceContext choiceContext,
+        CombatSide side,
+        IEnumerable<Creature> participants
+    )
+    {
+        if (Owner?.Side == side)
+        {
+            await PowerCmd.Remove(this);
+        }
+    }
+}
