b7a657c4ee421822c1148661f370c3d3fcd8a23f
青白影 <376565306@qq.com>
2026-06-19 00:31:04 +0800

    白血骑姬 PaleBloodRider

---
+++ RebelliaCode/Cards/Uncommon/PaleBloodRider.cs
@@ -0,0 +1,28 @@
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using Rebellia.RebelliaCode.Api;
+using Rebellia.RebelliaCode.Api.Cards;
+using Rebellia.RebelliaCode.Api.DynamicVars;
+using Rebellia.RebelliaCode.Powers.cards;
+
+namespace Rebellia.RebelliaCode.Cards.Uncommon;
+
+public class PaleBloodRider()
+    : RebelliaCard(2, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+        [new PowerVar<PaleBloodRiderPower>(1), new PowerVar<PaleBloodRiderUpgradedPower>(1)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        if (IsUpgraded)
+        {
+            await Utils.GivePower<PaleBloodRiderUpgradedPower>(choiceContext, this, play);
+        }
+        else
+        {
+            await Utils.GivePower<PaleBloodRiderPower>(choiceContext, this, play);
+        }
+    }
+}
---
+++ RebelliaCode/Powers/cards/PaleBloodRiderPower.cs
@@ -0,0 +1,55 @@
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using Rebellia.RebelliaCode.Api.Extensions;
+using Rebellia.RebelliaCode.Api.Powers;
+
+namespace Rebellia.RebelliaCode.Powers.cards;
+
+public class PaleBloodRiderPower : RebelliaPowers
+{
+    private const int DrawCount = 1;
+
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    public override int DisplayAmount => DrawCount;
+    public override bool ShouldReceiveCombatHooks => true;
+    public override PowerInstanceType InstanceType => PowerInstanceType.Instanced;
+
+    public override async Task AfterSideTurnStart(
+        CombatSide side,
+        IReadOnlyList<Creature> participants,
+        ICombatState combatState
+    )
+    {
+        if (side != Owner.Side)
+            return;
+        if (Owner.Player == null)
+            return;
+
+        var player = Owner.Player;
+
+        var allBloodWeapons = PileType
+            .Draw.GetPile(player)
+            .Cards.Concat(PileType.Hand.GetPile(player).Cards)
+            .Concat(PileType.Discard.GetPile(player).Cards)
+            .Where(c => c.Tags.Contains(CardTagExtensions.RebelliaBloodWeapon))
+            .ToList();
+
+        if (allBloodWeapons.Count == 0)
+            return;
+
+        var toPlay = allBloodWeapons
+            .OrderBy(_ => player.RunState.Rng.CombatCardSelection.NextInt())
+            .Take(DrawCount)
+            .ToList();
+
+        foreach (var card in toPlay)
+        {
+            await CardCmd.AutoPlay(new BlockingPlayerChoiceContext(), card, null);
+        }
+    }
+}
