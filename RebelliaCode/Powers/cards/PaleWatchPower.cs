0e0ec7a7ad77cc2cae683436ed9ee940fad718dc
青白影 <376565306@qq.com>
2026-07-02 23:18:32 +0800

    苍白守望修复

--- RebelliaCode/Powers/cards/PaleWatchPower.cs
+++ RebelliaCode/Powers/cards/PaleWatchPower.cs
@@ -2,7 +2,9 @@ using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using Rebellia.RebelliaCode.Api.Extensions;
 using Rebellia.RebelliaCode.Api.Powers;
 
@@ -14,24 +16,44 @@ public class PaleWatchPower : RebelliaPowers
     public override PowerStackType StackType => PowerStackType.Counter;
     public override bool ShouldReceiveCombatHooks => true;
 
-    public override async Task AfterSideTurnStart(
+    protected override object InitInternalData() => new Data();
+
+    private class Data
+    {
+        public int BloodWeaponCount { get; set; }
+    }
+
+    private Data GetData() => GetInternalData<Data>();
+
+    public override async Task BeforeSideTurnEndEarly(
+        PlayerChoiceContext choiceContext,
         CombatSide side,
-        IReadOnlyList<Creature> participants,
-        ICombatState combatState
+        IEnumerable<Creature> participants
     )
     {
         if (side != Owner.Side)
             return;
 
-        var player = Owner.Player;
-        if (player == null)
+        var handPile = PileType.Hand.GetPile(Owner.Player!);
+        var count = handPile.Cards.Count(c =>
+            c.Tags.Contains(CardTagExtensions.RebelliaBloodWeapon)
+        );
+        GetData().BloodWeaponCount = count;
+
+        await Task.CompletedTask;
+    }
+
+    public override async Task AfterPlayerTurnStart(
+        PlayerChoiceContext choiceContext,
+        Player player
+    )
+    {
+        if (player != Owner.Player)
             return;
 
-        var bloodWeaponCount = PileType
-            .Hand.GetPile(player)
-            .Cards.Count(c => c.Tags.Contains(CardTagExtensions.RebelliaBloodWeapon));
-
-        await PlayerCmd.GainEnergy(bloodWeaponCount, player);
+        int count = GetData().BloodWeaponCount;
+        if (count > 0)
+            await PlayerCmd.GainEnergy(count, player);
         await PowerCmd.Decrement(this);
     }
 }
